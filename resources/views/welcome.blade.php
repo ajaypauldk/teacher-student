bdb55594a7fd28c67b7016a7bc282a474553f8bf
Ajay Kumar <ajay@live.dk>
2018-07-12 14:53:13 +0200

    added sub heading in master blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <h1>--Teacher-Student Management---</h1>
             </div>
         </div>
     </body>
